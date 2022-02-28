9a82ee121a107504710b31636720149d621b2d06
Akash Butala <akash.butala.365@gmail.com>
2022-02-28 12:02:51 +0530

    first commit

---
+++ src/components/Details/Details.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Card, CardHeader, CardContent, Typography } from '@material-ui/core';
+import { Doughnut } from 'react-chartjs-2';
+import useStyles from './styles';
+import useTransactions from '../../useTransactions';
+
+
+const DetailsCard = ({ title, subheader }) => {
+  const { total, chartData } = useTransactions(title);
+  const classes = useStyles();
+  console.log(chartData)
+  
+  return (
+    <Card className={title === 'Income' ? classes.income : classes.expense}>
+      <CardHeader title={title} subheader={subheader} />
+      <CardContent>
+        <Typography variant="h5">${total}</Typography>
+        <Doughnut data={chartData} height={10} width={10} />
+      </CardContent>
+    </Card>
+  );
+};
+
+export default DetailsCard;
+
+
